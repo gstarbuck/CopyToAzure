eb7a56b983b45f74f5987bcda2652e565877e079
Guy Starbuck <gstarbuck@hotmail.com>
2013-04-04 14:54:49 -0500

    Initial Commit

---
+++ CopyToAzure/AzureStorageHelper.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyToAzure
+{
+    internal class AzureStorageHelper
+    {
+        private class AzureStorageConstants
+        {
+            public const string Key = "xxxx";
+            public const string SharedKeyAuthorizationScheme = "SharedKey";
+            public const string Account = "xxxx";
+            public const string BlobEndPoint = "http://xxxxx.blob.core.windows.net/";
+        }
+
+        public String CreateAuthorizationHeader(String canonicalizedString)
+        {
+            String signature;
+            //UTF8Encoding utf8Encoding = new UTF8Encoding();
+            //byte[] storageKey = utf8Encoding.GetBytes(AzureStorageConstants.Key);
+            byte[] storageKey = Convert.FromBase64String(AzureStorageConstants.Key);
+
+            using (var hmacSha256 = new HMACSHA256(storageKey))
+            {
+                Byte[] dataToHmac = System.Text.Encoding.UTF8.GetBytes(canonicalizedString);
+                signature = Convert.ToBase64String(hmacSha256.ComputeHash(dataToHmac));
+            }
+
+            var authorizationHeader = String.Format(
+                CultureInfo.InvariantCulture,
+                "{0} {1}:{2}",
+                AzureStorageConstants.SharedKeyAuthorizationScheme,
+                AzureStorageConstants.Account,
+                signature);
+
+            return authorizationHeader;
+        }
+
+        public void PutBlob(String containerName, String blobName)
+        {
+            const string requestMethod = "PUT";
+
+            var urlPath = String.Format("{0}/{1}", containerName, blobName);
+
+            const string storageServiceVersion = "2011-08-18"; // "2009-09-19";
+
+            var dateInRfc1123Format = DateTime.UtcNow.ToString("R", CultureInfo.InvariantCulture);
+
+            var content = "The Name of This Band is Talking Heads";
+            var utf8Encoding = new UTF8Encoding();
+            var blobContent = utf8Encoding.GetBytes(content);
+            var blobLength = blobContent.Length;
+
+            const String blobType = "BlockBlob";
+
+
+            var canonicalizedHeaders = String.Format(
+                "x-ms-blob-type:{0}\nx-ms-date:{1}\nx-ms-version:{2}",
+                blobType,
+                dateInRfc1123Format,
+                storageServiceVersion);
+            /*
+            String canonicalizedHeaders = String.Format(
+             "x-mx-blob-content-length:{3}\nx-ms-blob-type:{0}\nx-ms-date:{1}\nx-ms-version:{2}",
+             blobType,
+             dateInRfc1123Format,
+             storageServiceVersion,
+             blobLength.ToString());
+            */
+            var canonicalizedResource = String.Format("/{0}/{1}", AzureStorageConstants.Account, urlPath);
+            var stringToSign = String.Format(
+                "{0}\n\n\n{1}\n\n\n\n\n\n\n\n\n{2}\n{3}",
+                requestMethod,
+                blobLength,
+                canonicalizedHeaders,
+                canonicalizedResource);
+            String authorizationHeader = CreateAuthorizationHeader(stringToSign);
+
+            var uri = new Uri(AzureStorageConstants.BlobEndPoint + urlPath);
+            var request = (HttpWebRequest) WebRequest.Create(uri);
+            request.Method = requestMethod;
+            request.Headers.Add("x-ms-blob-type", blobType);
+            request.Headers.Add("x-ms-date", dateInRfc1123Format);
+            request.Headers.Add("x-ms-version", storageServiceVersion);
+            request.Headers.Add("Authorization", authorizationHeader);
+            request.ContentLength = blobLength;
+
+            try
+            {
+                using (var requestStream = request.GetRequestStream())
+                {
+                    requestStream.Write(blobContent, 0, blobLength);
+                }
+
+                using (var response = (HttpWebResponse) request.GetResponse())
+                {
+                    String ETag = response.Headers["ETag"];
+                }
+            }
+            catch (WebException webEx)
+            {
+                if (webEx != null)
+                {
+                    var resp = webEx.Response;
+                    if (resp != null)
+                    {
+                        using (var sr = new StreamReader(resp.GetResponseStream(), true))
+                        {
+                            var responseText = sr.ReadToEnd();
+                                //This is where details about this 403 message can be found
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ CopyToAzure/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace CopyToAzure
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            string storageAccountConnectionString;
+            string blobContainerName;
+            string baseDirectoryPath;
+
+            try
+            {
+                storageAccountConnectionString = args[0];
+                blobContainerName = args[1];
+                baseDirectoryPath = args[2];
+            }
+            catch
+            {
+                Console.WriteLine("Usage: CopyToAzure [storage connection string] [blob container name] [base directory path]");
+            }
+
+            // Load up the storage account from your connection string
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                ConfigurationManager.ConnectionStrings["StorageConnectionString"].ConnectionString);
+
+            // Create the blob client
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            //Retrieve a reference to the container
+            //TODO: replace hardcoded container name with command line argument
+            CloudBlobContainer blobContainer = blobClient.GetContainerReference("darcdev");
+
+            //Create the container if it doesn't already exist
+            blobContainer.CreateIfNotExists();
+
+            //???
+            blobContainer.SetPermissions(new BlobContainerPermissions{PublicAccess=BlobContainerPublicAccessType.Container});
+
+
+            // Now recurse through the submitted directory and upload all files
+            //TODO: replace directory with command line reference
+            var baseDirectoryPath = @"C:\dev\MMSD\DARC-WPF\Applications\WPF\DarcWpfClient\DarcWpfClient\publish\";
+            ProcessFolder(baseDirectoryPath, baseDirectoryPath, blobContainer);
+            Console.WriteLine("Done!");
+        }
+
+        private static void ProcessFolder(string baseDirectoryPath, string folderToProcess, CloudBlobContainer blobContainer)
+        {
+            var directory =
+                new DirectoryInfo(folderToProcess);
+            foreach (FileSystemInfo info in directory.EnumerateFileSystemInfos())
+            {
+                // Determine if entry is a directory or a file
+                //if ((info.Attributes & FileAttributes.Directory) != FileAttributes.Directory)
+                if ((info.Attributes & FileAttributes.Directory) == FileAttributes.Directory)
+                {
+                    ProcessFolder(baseDirectoryPath, info.FullName, blobContainer);
+                }
+                else
+                {
+                    UploadFile(baseDirectoryPath, info.FullName, blobContainer);
+                }
+            }
+        }
+
+        private static void UploadFile(string baseDirectoryPath, string fullFilePath, CloudBlobContainer blobContainer)
+        {
+            var trimmedPath = fullFilePath.Replace(baseDirectoryPath, string.Empty);
+            var name = trimmedPath;
+            if (trimmedPath.Contains(@"\"))
+            {
+                name = SwapSlashes(trimmedPath);
+            }
+            CloudBlockBlob blob = blobContainer.GetBlockBlobReference(name);
+            using (var fileStream = File.OpenRead(fullFilePath))
+            {
+                blob.UploadFromStream(fileStream);
+            }
+            Console.WriteLine(trimmedPath);
+        }
+
+        private static string SwapSlashes(string inString)
+        {
+            return inString.Replace(@"\", "/");
+        }
+    }
+}
